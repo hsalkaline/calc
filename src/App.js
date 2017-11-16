2a885a72041ee523ee694860ed5c5763be151fd4
Valeriya Bezrukova <bezekler@95.108.185.81-red.dhcp.yndx.net>
2017-11-16 11:54:54 +0300

    сумма 2-х чисел

--- src/App.js
+++ src/App.js
@@ -2,20 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function sum (a,b) {
+    return(a+b);
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput1: '',
+            userInput2: '',
+            sum: '0'
+        };
+    }
+
+    handleUserInput1 = (e) => {
+        this.setState({
+            userInput1: e.target.value
+        });
+    };
+
+    handleUserInput2 = (e) => {
+        this.setState({
+            userInput2: e.target.value
+        });
+    };
+
+    calc = () => {
+        this.setState({ sum: +this.state.userInput1 + +this.state.userInput2 });
+    };
+
+    render() {
+        return (
+            <div className="App">
+
+                <form>
+                    <label>
+                        <p>Var 1:
+                            <input onChange={this.handleUserInput1} value1={this.state.userInput1}/>
+                        </p>
+                        <p>
+                            Var 2:
+                            <input onChange={this.handleUserInput2} value2={this.state.userInput2}/>
+                        </p>
+                    </label>
+
+                </form>
+                <p>
+                    <button onClick={this.calc}>
+                        Sum
+                    </button>
+                </p>
+                <h1>
+                    {this.state.sum}
+                </h1>
+            </div>
+        );
+    }
 }
 
 export default App;
